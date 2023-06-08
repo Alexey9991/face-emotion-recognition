f31c3c9eb96df1422734a1f8f5c91999c3233612
== <=>
2023-06-08 16:33:02 +0300

    emotion recognition

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,49 +1,39 @@
-import cv2
 import torch
-from torchvision.transforms import transforms
-import numpy as np
+from torchvision import transforms
+from PIL import Image
+from train import EmotionClassifier
 
-# Загрузка модели классификатора эмоций
-model = torch.load('emotion_classifier.pt')
+# Определение модели
+model = EmotionClassifier()
 
+# Загрузка предварительно обученной модели
+state_dict = torch.load('emotion_classifier.pt')
+model.load_state_dict(state_dict)
+model.eval()
 
+# Загрузка и предобработка изображения
+image_path = './data_for_recognition/1.jpg'
+image = Image.open(image_path)
+preprocess = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((48, 48)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
+])
+input_tensor = preprocess(image)
+input_batch = input_tensor.unsqueeze(0)
 
-# Список классов эмоций
-#emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
-emotion_labels = ['Angry']
+# Предсказание класса изображения
+with torch.no_grad():
+    output = model(input_batch)
 
-# Загрузка каскадного классификатора для определения лица
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Загрузка файла с классами
+with open('emotion_classes.txt', 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
 
-# Загрузка изображения
-image = cv2.imread('data_for_recognition/face-people-portrait-actor-hair-Person-Nicolas-Cage-man-beard-look-male-hairstyle-facial-hair-bristle-579421.jpg')
+# Получение предсказанного класса
+_, predicted_idx = torch.max(output, 1)
+predicted_class = classes[predicted_idx.item()]
 
-# Преобразование изображения в градации серого
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Обнаружение лица на изображении
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-# Обработка каждого обнаруженного лица
-for (x, y, w, h) in faces:
-    # Извлечение области интереса (лица) изображения
-    roi_gray = gray[y:y + h, x:x + w]
-    roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-    
-    # Преобразование области интереса в тензор PyTorch
-    transform = transforms.Compose([transforms.ToTensor()])
-    roi = transform(roi_gray).unsqueeze(0)
-    
-    # Классификация эмоции
-    with torch.no_grad():
-        prediction = model(roi)
-    emotion_label = emotion_labels[prediction.argmax().item()]
-    
-    # Отображение результата на изображении
-    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    cv2.putText(image, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-print(emotion_label)
-# Отображение и сохранение результата
-cv2.imshow('Emotion Detection', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Вывод предсказанного класса
+print(f'Predicted class: {predicted_class}')
